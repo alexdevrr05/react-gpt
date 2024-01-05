43ac341012269b70b2459f222976e32e284967b9
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-01-04 20:54:52 -0600

    Add TypingLoader component to OrthographyPage (suspensive dots)

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,13 +1,16 @@
-import { GptMessage, MyMessage } from '../../components';
+import { GptMessage, MyMessage, TypingLoader } from '../../components';
 
 export const OrthographyPage = () => {
   return (
     <div className='chat-container'>
       <div className='chat-messages'>
         <div className='grid grid-cols-12 gap-y-2'>
-          {/* Bienvenida */}
+          {/* Chat izquierda */}
           <GptMessage text='Hola, escribe tu texto en español y te ayudo con las correcciones' />
+          {/* Chat derecha */}
           <MyMessage text='Hola, soy Alex' />
+          {/* Puntos suspensivos */}
+          <TypingLoader className='fade-in' />
         </div>
       </div>
     </div>
