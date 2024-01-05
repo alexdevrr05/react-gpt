a587d6cc6486713a39f1bb428e0fe81262416579
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-01-04 18:57:29 -0600

    Add react-markdown dependency and new chat components
    Update OrthographyPage with chat messages design

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,3 +1,15 @@
+import { GptMessage, MyMessage } from '../../components';
+
 export const OrthographyPage = () => {
-  return <div>OrthographyPage</div>;
+  return (
+    <div className='chat-container'>
+      <div className='chat-messages'>
+        <div className='grid grid-cols-12 gap-y-2'>
+          {/* Bienvenida */}
+          <GptMessage text='Hola, escribe tu texto en español y te ayudo con las correcciones' />
+          <MyMessage text='Hola, soy Alex' />
+        </div>
+      </div>
+    </div>
+  );
 };
